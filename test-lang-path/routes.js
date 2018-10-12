6fa0475b44e3dabc83cf6324d176d8eb8a2ec247
panupong <tayalone@gmail.com>
2018-10-12 10:16:32 +0700

    copy from i18n project

--- test-lang-path/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = require('next-routes');
-
-// define named routes
-module.exports = routes()
-  .add('about')
-  .add('article', '/article/:slug')
-  .add('testlang', '/:countryCode/:lang/testlang');
